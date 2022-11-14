950faa32901e11c1e0b87b1f935f0e45d92fd473
Stanislava Filonova <stanislavafilonova@yandex.ru>
2022-11-14 10:04:49 +0300

    работа над дизайном калькулятора

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -4,33 +4,33 @@ import './Calculator.css';
 
 function Calculator () {
     return (
-    <div class="calculator-grid">
-            <div class="output">
+    <div className="calculator-grid">
+            <div className="output">
                 {/* Отображение ранее введенных цифр */}
-                <div class="previous-operand"></div>
+                <div className="previous-operand">123 +</div>
                 {/* Отображение последней введенной цифры */}
-                <div class="current-operand"></div>
+                <div className="current-operand">456</div>
             </div>
             {/* Отображение кнопок калькулятора */}
-            <button>AC</button>
-            <button>+/-</button>
-            <button>%</button>
-            <button>÷</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>×️</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>-</button>
-            <button>.</button>
-            <button class="span-two">0</button>
-            <button>=</button>
+            <button className="dark-button">AC</button>
+            <button className="dark-button">+/-</button>
+            <button className="dark-button">%</button>
+            <button className="orange-button">÷</button>
+            <button className="simple-button">7</button>
+            <button className="simple-button">8</button>
+            <button className="simple-button">9</button>
+            <button className="orange-button">×️</button>
+            <button className="simple-button">4</button>
+            <button className="simple-button">5</button>
+            <button className="simple-button">6</button>
+            <button className="orange-button">+</button>
+            <button className="simple-button">1</button>
+            <button className="simple-button">2</button>
+            <button className="simple-button">3</button>
+            <button className="orange-button">-</button>
+            <button className="span-two">0</button>
+            <button className="simple-button">.</button>
+            <button className="orange-button">=</button>
     </div>
 )
 };
